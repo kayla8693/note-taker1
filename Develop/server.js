82f3bcf8be7fdb1f89c7316aeeeab45e441aed65
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-03-11 18:54:11 -0500

    updated readme, added screenshots

--- Develop/server.js
+++ Develop/server.js
@@ -5,8 +5,7 @@ const PORT = 4000;
 const app = express();
 let savedNotes = require('./db/db.json');
 
-
-// serves static files
+// Serves static files
 app.use(express.static(__dirname + '/public'));
 
 app.use(express.urlencoded({ extended: true }));
@@ -14,36 +13,37 @@ app.use(express.json());
 
 const output_dir = path.resolve(__dirname, "public");
 
-// Routes============================================================================
-
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(output_dir, 'index.html'));
-});
-
-app.get('/notes', function (req, res) {
-    res.sendFile(path.join(output_dir, 'notes.html'));
-});
-
-
-app.get('/api/notes', function (req, res) {
-    return res.json(savedNotes);
-
-});
-
-
+// Function to write notes to db.json
 function writeNotes() {
     fs.writeFileSync('db/db.json', JSON.stringify(savedNotes), function (err) {
         if (err) {
             return err;
         }
-        console.log('note saved!');
 
     });
-}
+};
 
+// Routes============================================================================
+
+// Gets homepage
+app.get('/', function (req, res) {
+    res.sendFile(path.join(output_dir, 'index.html'));
+});
+
+// Gets notes page
+app.get('/notes', function (req, res) {
+    res.sendFile(path.join(output_dir, 'notes.html'));
+});
+
+// Displays all notes in json
+app.get('/api/notes', function (req, res) {
+    return res.json(savedNotes);
+});
+
+// Posts created notes to /api/notes; writes new notes to db.json
 app.post('/api/notes', function (req, res) {
     var note = req.body;
+    // Gives note a unique id
     note.id = note.title.replace(/\s+/g, "").toLowerCase();
     console.log(note);
 
@@ -54,46 +54,21 @@ app.post('/api/notes', function (req, res) {
     return res.json(savedNotes);
 });
 
+// Deletes note based on id, rewrites db.json w updated notes
 app.delete("/api/notes/:id", function (req, res) {
     var id = req.params.id;
     console.log(id);
 
     for (var i = 0; i < savedNotes.length; i++) {
         if (savedNotes[i].id === id) {
-            console.log(savedNotes[i].id)
-            console.log(savedNotes[i])
             savedNotes.splice(i, 1);
             writeNotes();
             return res.json(savedNotes);
-
         }
-        
-    }
-
-
-})
-
-
-
-
-// In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    };
+});
 
 // Create listener on port 3000
 app.listen(PORT, function () {
     console.log('APP is listening at http://localhost:' + PORT);
-})
+});
